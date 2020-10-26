8e363a941d6e852b35aed1737bfc11b23f3f98c2
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-26 21:30:41 +0300

    Add ChannelAdapter class

---
+++ app/src/main/java/by/training/rssreader/adapter/ChannelAdapter.kt
@@ -0,0 +1,31 @@
+package by.training.rssreader.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import by.training.rssreader.R
+import by.training.rssreader.entity.NewsChannel
+
+class ChannelAdapter : RecyclerView.Adapter<ChannelViewHolder>() {
+
+    private var channels = mutableListOf<NewsChannel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChannelViewHolder {
+        return ChannelViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ChannelViewHolder, position: Int) {
+        holder.bind(channels[position])
+    }
+
+    override fun getItemCount(): Int {
+        return channels.size
+    }
+
+    fun setData(data: List<NewsChannel>) {
+        channels = data as MutableList<NewsChannel>
+        notifyDataSetChanged()
+    }
+}
